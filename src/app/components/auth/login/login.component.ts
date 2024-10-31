41585a0da30c0d2713bdd4a4db09efe796d54336
linhld <baclinh0123@gmail.com>
2024-10-31 17:05:53 +0700

    udpate

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthService } from '../../../services/auth/auth.service';
+
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -10,12 +13,25 @@ import { FormsModule } from '@angular/forms';
 export class LoginComponent {
   username: string = '';
   password: string = '';
+  errorMessage: string = '';
+
+  constructor(private authService: AuthService) { }
+
 
   onSubmit() {
     // Xử lý đăng nhập ở đây
     console.log('Đăng nhập với email:', this.username);
     console.log('Đăng nhập với mật khẩu:', this.password);
     // Thực hiện logic đăng nhập (gọi service, xác thực, v.v.)
+    this.authService.login(this.username, this.password).subscribe(
+      (response) => {
+        console.log('Đăng nhập thành công:', response);
+      },
+      (error) => {
+        console.error('Đăng nhập thất bại:', error);
+        this.errorMessage = 'Đăng nhập thất bại. Vui lòng kiểm tra lại thông tin.';
+      }
+    );
   }
 
 }
